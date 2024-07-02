24c1ab338606a096becfa69ab8c982c673d4d9f5
정우혁 <alalalalalalall367@gmail.com>
2024-07-02 20:38:21 +0900

    간단한 Post 엔티티 설계

---
+++ src/main/java/com/springstudy/myspringstudy/domain/Post.java
@@ -0,0 +1,24 @@
+package com.springstudy.myspringstudy.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Lob
+    private String content;
+    @Builder
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
