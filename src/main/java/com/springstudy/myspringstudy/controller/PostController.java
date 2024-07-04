7c0674b483177a2f999727bb108a8ef2f8b66e77
정우혁 <alalalalalalall367@gmail.com>
2024-07-04 16:06:08 +0900

    PostController 반환값 변경

--- src/main/java/com/springstudy/myspringstudy/controller/PostController.java
+++ src/main/java/com/springstudy/myspringstudy/controller/PostController.java
@@ -1,7 +1,7 @@
 package com.springstudy.myspringstudy.controller;
 
-import com.springstudy.myspringstudy.domain.Post;
 import com.springstudy.myspringstudy.dto.request.PostCreate;
+import com.springstudy.myspringstudy.dto.response.PostResponse;
 import com.springstudy.myspringstudy.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +21,7 @@ public class PostController {
     }
 
     @GetMapping("/posts/{postId}")
-    public Post get(@PathVariable("postId") Long id) {
+    public PostResponse get(@PathVariable("postId") Long id) {
         return postService.get(id);
     }
 }
