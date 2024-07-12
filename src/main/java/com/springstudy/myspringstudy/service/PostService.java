5f280007566f6b40a6a05b1ea6f5f123402e7eed
정우혁 <alalalalalalall367@gmail.com>
2024-07-12 11:31:09 +0900

    PostService 내부 전체조회 로직 설계

--- src/main/java/com/springstudy/myspringstudy/service/PostService.java
+++ src/main/java/com/springstudy/myspringstudy/service/PostService.java
@@ -2,12 +2,16 @@ package com.springstudy.myspringstudy.service;
 
 import com.springstudy.myspringstudy.domain.Post;
 import com.springstudy.myspringstudy.dto.request.PostCreate;
+import com.springstudy.myspringstudy.dto.request.PostSearch;
 import com.springstudy.myspringstudy.dto.response.PostResponse;
 import com.springstudy.myspringstudy.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -28,12 +32,15 @@ public class PostService {
         Post post = postRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("글이 존재하지 않습니다."));
 
-        PostResponse response = PostResponse.builder()
+        return PostResponse.builder()
                 .title(post.getTitle())
                 .id(post.getId())
                 .content(post.getContent())
                 .build();
+    }
 
-        return response;
+    public List<PostResponse> findAll(PostSearch postSearch) {
+        return postRepository.getList(postSearch).stream().map(PostResponse::new)
+                .collect(Collectors.toList());
     }
 }
