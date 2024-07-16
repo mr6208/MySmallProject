e078c5cf1ff9aa6e38c68e7f5d3430d2caf7cfe5
정우혁 <alalalalalalall367@gmail.com>
2024-07-16 22:29:13 +0900

    ExceptionController 내부 MySuperException에 대한 에러를 핸들링 하기위한 메서드 설계

--- src/main/java/com/springstudy/myspringstudy/controller/ExceptionController.java
+++ src/main/java/com/springstudy/myspringstudy/controller/ExceptionController.java
@@ -1,8 +1,10 @@
 package com.springstudy.myspringstudy.controller;
 
 import com.springstudy.myspringstudy.dto.response.ErrorResponse;
+import com.springstudy.myspringstudy.exception.MySuperException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,6 +20,7 @@ public class ExceptionController {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseBody
     public ErrorResponse invalidRequestHandler(MethodArgumentNotValidException e) {
+
         ErrorResponse response = ErrorResponse.builder()
                 .code("400")
                 .message("잘못된 요청입니다.")
@@ -28,4 +31,20 @@ public class ExceptionController {
         }
         return response;
     }
+
+    @ResponseBody
+    @ExceptionHandler(MySuperException.class)
+    public ResponseEntity<ErrorResponse> mySuperEx(MySuperException e) {
+        int statusCode = e.getStatusCode();
+
+        ErrorResponse response = ErrorResponse.builder()
+                .code(String.valueOf(statusCode))
+                .message(e.getMessage())
+                .validation(e.getValidation())
+                .build();
+        // 응답 json validation -> title : 제목에 '나쁜말'은 안돼요
+
+        return ResponseEntity.status(statusCode).body(response);
+    }
 }
+
