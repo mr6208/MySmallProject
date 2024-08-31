80fa1dfc11dc9ed646dcddb410d6767b7d475e9b
정우혁 <alalalalalalall367@gmail.com>
2024-08-31 17:32:45 +0900

    feat : UserDetails의 커스텀 구현체인 MemberDetails 클래스 설계

---
+++ src/main/java/com/springstudy/myspringstudy/dto/Member/request/MemberDetails.java
@@ -0,0 +1,61 @@
+package com.springstudy.myspringstudy.dto.Member.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Getter
+@RequiredArgsConstructor
+public class MemberDetails implements UserDetails {
+    private final String email;
+    private final String password;
+    private final String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return role;
+            }
+        });
+
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
