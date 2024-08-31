8ae3f142b316078325d110a06dd8bb49d3d08306
정우혁 <alalalalalalall367@gmail.com>
2024-08-31 17:29:50 +0900

    feat : 회원가입에 필요한 RequestDTO 설계

---
+++ src/main/java/com/springstudy/myspringstudy/dto/Member/request/JoinRequest.java
@@ -0,0 +1,20 @@
+package com.springstudy.myspringstudy.dto.Member.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JoinRequest {
+    @Email(message = "이메일 형식이 아닙니다")
+    @NotBlank(message = "이메일을 입력하세요")
+    private String email;
+
+    @NotBlank(message = "비밀번호를 입력하세요")
+    private String password;
+
+    @NotBlank(message = "이름을 입력해주세요")
+    private String name;
+}
